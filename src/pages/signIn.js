fc3475c3a18e13e2ea943a31c11aad459c3e93f8
sudipta9 <sudiptapradhan58271@gmail.com>
2022-05-02 08:26:51 +0530

    __

--- src/pages/signIn.js
+++ src/pages/signIn.js
@@ -1,24 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { Form, FormInput } from "../components";
 
 const SignInContainer = () => {
+  const [email, setEmail] = useState("");
+  const [emailMessage, setEmailMessage] = useState("");
+  const [isEmailValid, setIsEmailValid] = useState(false);
+  const [password, setPassword] = useState("");
+  const [passwordMessage, setPasswordMessage] = useState("");
+  const [isPasswordValid, setIsPasswordValid] = useState(false);
   return (
     <Form.Container backgroundImage="/images/misc/background.jpg">
-      <Form
-        onSubmit={(e) => {
-          console.log("submitting form");
-          e.preventDefault();
-        }}
-      >
+      <Form>
         <Form.Heading>Sign In</Form.Heading>
-        <FormInput.Container>
+        <FormInput.Container required>
           <FormInput.Label>Email Address</FormInput.Label>
-          <FormInput type="email" important />
+          <FormInput
+            type="email"
+            name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </FormInput.Container>
-        <FormInput.Container>
+        <FormInput.Container required>
           <FormInput.Label>Password</FormInput.Label>
-          <FormInput type="password" important />
+          <FormInput
+            type="password"
+            name="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </FormInput.Container>
         <Form.SubmitButton type="submit">Sign In</Form.SubmitButton>
         <Link to="/sign-up">Register</Link> |{" "}
